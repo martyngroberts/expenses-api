71015f76e89a49225b3dde7d56887a38277a5c05
martyngroberts <martyngroberts@users.noreply.github.com>
2021-02-18 10:39:05 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -48,11 +48,40 @@ app.get("/total", (req, res) => {
 });
 
 // 👆 public routes above 👆
-app.use(auth());
+app.use(
+ auth({
+   secret: SESSION_SECRET,
+   authRequired: false,
+   auth0Logout: true,
+   baseURL: APP_URL,
+   // 👇 add this 👇
+   authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
+   // 👆 add this 👆
+ })
+);
 // 👇 private routes below 👇
 
-app.get("/reports", (req, res) => {
-  res.send(expenses);
+app.get("/expenses", requiresAuth(), async (req, res, next) => {
+ try {
+   // 👇 get the token from the request 👇
+   const { token_type, access_token } = req.oidc.accessToken;
+   // 👇 then send it as an authorization header 👇
+   const expenses = await axios.get(`${API_URL}/reports`, {
+     headers: {
+       Authorization: `${token_type} ${access_token}`,
+     },
+   });
+   // 👆 end of changes 👆
+   res.render("expenses", {
+     user: req.oidc && req.oidc.user,
+     expenses: expenses.data,
+   });
+ } catch (err) {
+   next(err);
+ }
 });
 
 createServer(app).listen(PORT, () => {
